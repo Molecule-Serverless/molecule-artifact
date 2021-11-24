a719bb94aed8b3edb921caa48292e0a874156814
Dong Du <dd_nirvana@sjtu.edu.cn>
2021-11-24 22:44:17 +0800

    scripts: refine build_all.sh
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- build_all.sh
+++ build_all.sh
@@ -5,13 +5,21 @@
 
 ## 1. XPU-shim first
 echo -e "\033[44;37m ======[Molecule Builder] Start building XPU-shim \033[0m"
-cd xpu-shim/src
+pushd xpu-shim/src > /dev/null 2>&1
 make -j8
 # create the shared dir used to communicate between XPU-shim and functions
 sudo mkdir -p /tmp/fifo_dir
-
+popd > /dev/null 2>&1
 echo -e "\033[42;37m ======[Molecule Builder] Building XPU-shim success \033[0m"
 
+## 2. XPU-shim first
+echo -e "\033[44;37m ======[Molecule Builder] Start building vsandbox \033[0m"
+pushd vsandbox-runtime > /dev/null 2>&1
+./autogen.sh
+./configure
+make -j8
+popd > /dev/null 2>&1
+echo -e "\033[42;37m ======[Molecule Builder] Building vsandbox success \033[0m"
 
 ## n. Pull necessary docker images
 echo -e "\033[44;37m ======[Molecule Builder] Start pulling AE used docker images \033[0m"
